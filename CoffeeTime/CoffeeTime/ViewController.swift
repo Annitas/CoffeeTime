6c67a9fd9064637b84c485a2b08009e16f707684
Annitas <39757257+Annitas@users.noreply.github.com>
2024-02-08 16:27:12 +0600

    Delete storyboard

---
+++ CoffeeTime/CoffeeTime/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoffeeTime
+//
+//  Created by Anita Stashevskaya on 08.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
