9bdf401e210737b73fc2aff471e9bd4ec01c7d3c
Annitas <39757257+Annitas@users.noreply.github.com>
2024-02-09 12:33:01 +0600

    Make registration and signup screen

--- CoffeeTime/CoffeeTime/ViewController.swift
+++ CoffeeTime/CoffeeTime/ViewController.swift
@@ -6,12 +6,26 @@
 //
 
 import UIKit
+import SnapKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    private let registrationView = RegistrationView()
+    private let signupView = SignUpView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
         // Do any additional setup after loading the view.
+    }
+    
+    private func setupView() {
+        view.addSubview(signupView)
+        signupView.snp.makeConstraints { make in
+            make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).offset(10)
+            make.bottom.equalTo(view)
+            make.left.equalTo(view)
+            make.right.equalTo(view)
+        }
     }
 
 
